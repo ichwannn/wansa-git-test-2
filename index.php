37a8a8116a1d1b2593c348d051d6373c57d6d38c
ichwannn <ichwansanusi101@gmail.com>
2022-09-26 20:12:05 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit necessitatibus ex similique laborum voluptatem, odit velit molestias atque sed beatae tempora optio et esse voluptas reprehenderit dicta aspernatur corporis sunt.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
